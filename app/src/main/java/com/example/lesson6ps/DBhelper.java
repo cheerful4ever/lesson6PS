4fedeaadb46851eeb29b15268b22df5e49c6681a
18016375 <18016375@myrp.edu.sg>
2020-05-28 23:01:29 +0800

    activities 1-1000000

--- app/src/main/java/com/example/lesson6ps/DBhelper.java
+++ app/src/main/java/com/example/lesson6ps/DBhelper.java
@@ -1,4 +1,79 @@
 package com.example.lesson6ps;
 
-public class DBhelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.lesson6ps.Task;
+
+import java.util.ArrayList;
+
+public class DBhelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VER = 1;
+    private static final String DATABASE_NAME = "tasks.db";
+
+    private static final String TABLE_TASK = "Task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TASK_NAME = "name";
+    private static final String COLUMN_DESCRIPTION = "description";
+
+    public DBhelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_TASK +  "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TASK_NAME + " TEXT,"
+                + COLUMN_DESCRIPTION + " TEXT )";
+        db.execSQL(createTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int
+            newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
+
+    public void insertTask(String name, String description){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TASK_NAME, name);
+        values.put(COLUMN_DESCRIPTION, description);
+        db.insert(TABLE_TASK, null, values);
+        db.close();
+    }
+
+    public ArrayList<Task> getTasks() {
+        //TODO return records in Java objects
+
+        ArrayList<Task> toDos = new ArrayList<Task>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_TASK_NAME + ", "
+                + COLUMN_DESCRIPTION
+                + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String date = cursor.getString(1);
+                String data = cursor.getString(2);
+                Task obj = new Task(id, date, data);
+                toDos.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return toDos;
+    }
+
 }
