9f102007c2ae974427e0961e16ddaf256d3d908e
18016375 <18016375@myrp.edu.sg>
2020-05-28 23:25:23 +0800

    activities 1-1000000

--- app/src/main/java/com/example/lesson6ps/AddActivity.java
+++ app/src/main/java/com/example/lesson6ps/AddActivity.java
@@ -2,13 +2,77 @@ package com.example.lesson6ps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
+    EditText etName, etDescription;
+    Button btnAdd, btnCancel;
+    Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        etName = (EditText) findViewById(R.id.etName);
+        etDescription = (EditText) findViewById(R.id.etDescription);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                String description = etDescription.getText().toString();
+                if (!name.isEmpty() && !description.isEmpty()) {
+                    DBhelper dbhelper = new DBhelper(AddActivity.this);
+                    dbhelper.insertTask(name, description);
+                }
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(AddActivity.this,
+                        BroadcastReceiver.class);
+                String[] info = {name, description};
+                intent.putExtra("info", info);
+                sendBroadcast(intent);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this, 12345,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+
+                etName.setText("");
+                etDescription.setText("");
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
     }
 }
